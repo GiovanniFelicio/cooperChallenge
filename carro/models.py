cf7325cdf8877f3b26263ef14a2b3c079e3d7445
Giovanni <gvnnfelicio@gmail.com>
2021-09-17 20:30:59 -0300

    feat: mapeamento das models Marca e Carro tarefa#00001

--- carro/models.py
+++ carro/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from marca.models import Marca
+
+
+class Carro(models.Model):
+    nome = models.CharField(name='nome', max_length=100, null=False)
+    km_por_galao = models.FloatField(name='km_por_galao')
+    cilindros = models.SmallIntegerField(name='cilindros')
+    cavalo_de_forca = models.SmallIntegerField(name='cavalo_de_forca')
+    peso = models.SmallIntegerField(name='peso')
+    aceleracao = models.SmallIntegerField(name='aceleracao')
+    ano = models.DateField(name='ano')
+    origem = models.CharField(name='origem', max_length=20)
+    marca = models.ForeignKey(Marca, name='marca_id', null=False, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'carro'
--- marca/models.py
+++ marca/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Marca(models.Model):
+
+    nome = models.CharField(max_length=100)
+    origem = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'marca'
